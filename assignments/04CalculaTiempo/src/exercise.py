fd0ffd6002b1c25d2cf0724ac23800f83ce8600b
valeriaaaen <A01740735@itesm.mx>
2021-08-14 20:42:58 +0000

    Tarea1

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,8 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
+    a = int(input("Dame tu edad: "))
+    b = int(input("Dame el año actual: "))
+    c = (b-a)+100
+    print("Cumplirás 100 años en el año:",c)
 
 
 if __name__ == '__main__':
--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    an = int(input("Dame el año de nacimiento: "))
+    aa = int(input("Dame el año actual: "))
+    l = (aa-an)/5
+    print("Los lustros que has vivido son:",l)
+
 
 
 
